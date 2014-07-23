70b07952047c620b39b7a695438b99516c34b64f
Remi Bergsma <github@remi.nl>
2014-07-23 21:28:02 +0200

    Create preCacheThumbnails.sh

---
+++ bin/preCacheThumbnails.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# generate thumbnails for all new images in the past 60mins
+find /var/www/media -name \*.jpg -cmin -60 -type f -printf "%f\n"| awk {'print "curl -s \"http://`/sbin/ifconfig eth0 | grep inet | cut -d: -f2| tr -d Bcast`/timthumb.php?src=media/" $1 "&w=200\" 2>&1 >/dev/null" '} | sh
